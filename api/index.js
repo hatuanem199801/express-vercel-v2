9f47f81d20dc2caddfc3e0369c90e03507d3f00a
Tuấn Hà <tuanha@Tuans-MacBook-Air.local>
2022-02-27 17:06:39 +0700

    Public image

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
-app.use("/api/image", express.static(path.join(__dirname, "public")));
+app.use('/static', express.static('public'))
 
 app.get('/api', (req, res) => {
   const now = new Date().toUTCString();
