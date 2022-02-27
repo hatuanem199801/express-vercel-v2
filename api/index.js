d18f7565f67c9f828018df06133ea8ddb67fe0fb
Tuấn Hà <tuanha@Tuans-MacBook-Air.local>
2022-02-27 16:44:56 +0700

    Initial Project

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const app = require('express')();
+
+app.get('/api', (req, res) => {
+  const now = new Date().toUTCString();
+  const path = `/api/item/${now}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
